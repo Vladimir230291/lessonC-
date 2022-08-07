86a2ad325e6fc93cf0c2c6176c5bdd7b85180289
Vladimir <lumen_08@mail.ru>
2022-08-07 22:13:50 +0300

    4я задача

---
+++ Home_work_04/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите положительное число ");
+string? numberStr = Console.ReadLine();
+var number = int.Parse(numberStr!);
+
+double rest;
+if (number > 0){
+    Console.WriteLine("Четные числа");
+for (int i = 1; i <= number; i++){
+    rest = i % 2;
+    if (rest == 0){
+        Console.Write($" {i}");
+    }
+}
+}
+else {
+    Console.WriteLine("Ввеено не верное число");
+}
