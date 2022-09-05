cfaa6d759eb3a7dd6bf34ccea92132de3e24eb87
Vladimir <lumen_08@mail.ru>
2022-09-05 18:31:28 +0300

    домашка 9

---
+++ Home_work9/home_wokr#2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения m и n. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от m до n.
+// m = 1; n = 15 -> 120
+// m = 4; n = 8. -> 30
+
+int Inputnumbers()
+
+{
+    Console.WriteLine("Введите целое число");
+    if (int.TryParse(Console.ReadLine(), out int n));
+    return n;
+}
+
+int SumNumbers(int m, int n, int sum = 0)
+{
+    if (m > n) return sum;
+    else
+    {
+        sum += m;
+        return SumNumbers(m + 1, n, sum);
+    }
+}
+int a = Inputnumbers();
+int b = Inputnumbers();
+Console.WriteLine(SumNumbers(a, b));
---
+++ Home_work9/home_work#1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+void PrintValue(int m, int n)
+{
+    if (m < n)
+    {
+        System.Console.Write($"{m++}, ");
+        PrintValue(m, n);
+    }
+    else System.Console.WriteLine(n);
+}
+int InputNumbers()
+{
+    System.Console.WriteLine("Введите число");
+    if (int.TryParse(Console.ReadLine(), out int num))
+    {
+        return num;
+    }
+    else Console.WriteLine(" некоректное значние ");
+    return 0;
+}
+
+int a = InputNumbers();
+int b = InputNumbers();
+PrintValue(a, b);
